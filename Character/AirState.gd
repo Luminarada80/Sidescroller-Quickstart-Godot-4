ab6daee3436093caa846e5ac29d0bab30dc0d975
Luminarada80 <140645994+Luminarada80@users.noreply.github.com>
2023-12-02 23:25:13 -0500

    Start of Attacks and Enemy Setup video
    Beginning of https://www.youtube.com/watch?v=NVAXjTzqTyE

--- Character/AirState.gd
+++ Character/AirState.gd
@@ -1,11 +1,28 @@
 extends State
 
+class_name AirState
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var landing_state : State
+@export var double_jump_velocity : float = -100
+@export var double_jump_animation : String = "double_jump"
+@export var landing_animation : String = "landing"
 
+var has_double_jumped = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func state_process(delta):
+	if(character.is_on_floor()):
+		next_state = landing_state
+
+func state_input(event : InputEvent):
+	if(event.is_action_pressed("jump") && not has_double_jumped):
+		double_jump()
+
+func on_exit():
+	if(next_state == landing_state):
+		playback.travel(landing_animation)
+		has_double_jumped = false
+
+func double_jump():
+	character.velocity.y = double_jump_velocity
+	playback.travel(double_jump_animation)
+	has_double_jumped = true
--- Character/LandingState.gd
+++ Character/LandingState.gd
@@ -1,11 +1,10 @@
 extends State
 
+@export var landing_animation_name : String = "landing"
+@export var ground_state : State
+@export var idle_animation_name : String = "idle"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == landing_animation_name):
+		playback.travel(idle_animation_name)
+		next_state = ground_state
